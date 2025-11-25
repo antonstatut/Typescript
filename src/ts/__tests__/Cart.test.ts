76555b59fc49994773cf3f1334e941ad923661df
Anton Statut <antonstatut@gmail.com>
2025-11-25 20:53:48 +0300

    исправил ошибки и добавил тесты

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,49 @@
 import Cart from '../service/Cart';
+import Book from '../domain/Book';
+import MusicAlbum from '../domain/MusicAlbum';
+import Movie from '../domain/Movie';
 
-test('new card should be empty', () => {
+test('создание новой пустой карты', () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
+});
+
+test('проверка сумирования без учета скидки', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+
+  const total = cart.calculateTotalWithoutDiscount();
+  expect(total).toBe(2900);
+});
+
+test('проверка сумирования с учетом скидки', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  
+  const total = cart.calculateTotalWithDiscount(10);
+  expect(total).toBe(2610);
+});
+
+test('Проверка корректности скидки', () => {
+  const cart = new Cart();
+
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  
+  expect(() => {cart.calculateTotalWithDiscount(150)}).toThrow('Некорректное значение скидки. Допустимый диапазон: 0-100.');
+
+});
+
+test('удаление обьекта по полю id', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+
+  cart.removeItemById(1001);
+
+  expect(cart.items.find(obj => obj.id === 1001)).toBe(undefined)
 });
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,35 +1,30 @@
 import Buyable from '../domain/Buyable';
 
 export default class Cart {
-    private _items: Buyable[] = [];
+  private _items: Buyable[] = [];
 
-    add(item: Buyable): void {
-        this._items.push(item);
-    }
+  add(item: Buyable): void {
+    this._items.push(item);
+  }
 
-    get items(): Buyable[] {
-        return [...this._items]; 
-    }
+  get items(): Buyable[] {
+    return [...this._items]; 
+  }
+    
+  calculateTotalWithoutDiscount(): number {
+    let sum = this._items.reduce((acc, item) => acc + item.price, 0);
+    return sum;
+  }
 
-    calculateTotalWithoutDiscount(): number {
-        let total: number = 0;
-        for (const item of this._items) {
-            total += item.price;
-        }
-        return total;
-    }
+  calculateTotalWithDiscount(discount: number): number {
+    let total = this.calculateTotalWithoutDiscount();
+      if (discount < 0 || discount > 100) {
+        throw new Error('Некорректное значение скидки. Допустимый диапазон: 0-100.');
+      }
+      return total = total * (1 - discount / 100);
+  }
 
-    calculateTotalWithDiscount(discount: number): number {
-        const total: number = this.calculateTotalWithoutDiscount();
-        if (discount < 0 || discount > 100) {
-            console.warn("Скидка должна быть в диапазоне от 0 до 100%");
-            return total;
-        }
-        const discountedTotal: number = total * (1 - discount / 100);
-        return discountedTotal;
-    }
-
-    removeItemById(id: number): void {
-        this._items = this._items.filter(item => item.id !== id);
-    }
+  removeItemById(id: number): void {
+    this._items = this._items.filter(item => item.id !== id);
+  }
 }
